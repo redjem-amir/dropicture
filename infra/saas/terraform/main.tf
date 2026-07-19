088f105e6bdf0de91f87eabeef3710eaaa67fb94
redjem-amir <a.redjem@outlook.com>
2026-07-19 20:46:48 +0200

    chore(saas): set an empty trusted key group on the cdn behavior

--- infra/saas/terraform/main.tf
+++ infra/saas/terraform/main.tf
@@ -555,6 +555,8 @@ resource "aws_cloudfront_distribution" "cdn" {
     cache_policy_id            = aws_cloudfront_cache_policy.cdn.id
     origin_request_policy_id   = data.aws_cloudfront_origin_request_policy.cors_s3.id
     response_headers_policy_id = aws_cloudfront_response_headers_policy.cdn.id
+
+    trusted_key_groups = []
   }
 
   restrictions {
